47074d5c686ea9561593c090a73ddc229c04a1bb
Bart Malfait <bart@auvifox.be>
2024-04-04 12:53:35 +0200

    added authenticationmessagehandler in client / update

--- BlazorWebAppOidc.Client/Program.cs
+++ BlazorWebAppOidc.Client/Program.cs
@@ -3,6 +3,7 @@ using BlazorWebAppOidc.Client.Weather;
 using BlazorWebAppOidc.Services.Http;
 using BlazorWebAppOidc.Services.Interfaces;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -16,9 +17,10 @@ builder.Services.AddHttpClient<IWeatherForecaster, ClientWeatherForecaster>(http
     httpClient.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
 });
 
+builder.Services.AddScoped<AuthorizationMessageHandler>();
 builder.Services.AddHttpClient<IArticleService, ArticleService>(httpClient =>
 {
     httpClient.BaseAddress = new Uri("https://localhost:7166/");
-});
+}).AddHttpMessageHandler<AuthorizationMessageHandler>();
 
 await builder.Build().RunAsync();
